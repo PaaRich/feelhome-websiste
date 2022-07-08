8320c2b84162f406efcc1cbf63cfd9df2aa3213a
PaaRich <100782573+PaaRich@users.noreply.github.com>
2022-07-08 12:56:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,15 @@
 <body>
     <header>
         <div>
+            <button id="menu-btn">Menu</button>
             <nav>
                 <div class="logo">
                     <span><span class="feel">Feel</span><span class="home">Home</span></span>
+                    <div id="toggle">X</div>
                 </div>
                 <div class="links">
                     <ul>
-                        <li><a class="first_child" href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
                         <li><a href="#">Services</a></li>
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Blog</a></li>
@@ -37,13 +39,11 @@
                     </h1>
                     <p>You are warmly welcome to comfortable Home</p>
                     <div>
-                        <button id="this">Read more<ion-icon name="arrow-forward-outline" style="margin-left:0.2em;"></ion-icon></button>
+                        <button id="this"><span>Read more</span><ion-icon name="arrow-forward-outline" style="margin-left:0.2em;"></ion-icon></button>
                         <button>Rate us</button>
                     </div>
                 </div>
-                <div class="whatsapp-btn">
-                    <ion-icon name="logo-whatsapp"></ion-icon>
-                </div>
+                <ion-icon id="whatsapp-logo" name="logo-whatsapp"></ion-icon>
             </div>
         </div>
     </header>
@@ -61,7 +61,7 @@
                     <!-- there are correction to be made here -->
                     <ion-icon name="school-outline" style="display: block;font-size:60px;color:black;margin: auto;"></ion-icon>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officia velit, et voluptas corporis molestiae hic, rem quidem sequi temporibus nesciunt. Repellat blanditiis ut facilis? Architecto repellendus eaque animi explicabo?</p>
-                    <button id="for_service">Read more<ion-icon name="arrow-forward-outline" style="margin-left: 0.2em;"></ion-icon></button>
+                    <button id="for_service"><span>Read more</span><ion-icon name="arrow-forward-outline" style="margin-left: 0.2em;"></ion-icon></button>
                 </div>
                 <div class="service_img">
                     <img src="resources/images/hug.jpg" alt="image">
@@ -79,51 +79,60 @@
             <h2>SERVICES</h2>
             <div class="main ">
                 <div class="text ">
-                    <h3>YOU'RE WELCOME TO A SECOND HOME</h3>
+                    <h3>YOU'RE WELCOME</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quibusdam, enim perferendis minima quas sunt vero reprehenderit eveniet pariatur voluptate ipsa asperiores expedita sed veniam libero, sit reiciendis magnam deleniti accusantium
-                        nemo praesentium blanditiis. Dolor officia totam ab aliquam autem magni quibusdam odio dolore, expedita possimus corrupti molestias natus rem?</p>
+                        nemo praesentium blanditiis. Dolor officia totam ab al?</p>
                 </div>
                 <div class="forms ">
                     <form>
-                        <div>
-                            <label>Name:</label>
-                            <input type="text " placeholder="SIEH RICHMOND " required>
-                        </div>
-                        <div>
-                            <label>Email:</label>
-                            <input type="email " placeholder="siehrichmond50@gmail.com " required>
-                        </div>
-                        <div>
-                            <label>Contact:</label>
-                            <input type="contact " placeholder="0546684004 " required>
-                        </div>
-                        <div>
-                            <label>Gender:</label>
-                            <select name="gender " id="gender " required>
-                                <option value="male ">Male</option>
-                                <option value="female ">Female</option>
-                            </select>
-                        </div>
-                        <div>
-                            <label>Hostel:</label>
-                            <select name="hostels " id=" " required>
-                                <option value="E-step ">E-STEP</option>
-                                <option value="forest ">FOREST</option>
-                                <option value="GET-FUND ">GET-FUND</option>
-                            </select>
-                        </div>
-                        <div>
-                            <label>level:</label>
-                            <select name="levels " id=" " required>
-                                <option value="L100 ">L100</option>
-                                <option value="L200 ">L200</option>
-                                <option value="L300 ">L300</option>
-                                <option value="L400 ">L400</option>
-                            </select>
-                        </div>
-                        <div>
-                            <input type="submit " placeholder="submit">
-                        </div>
+                        <table>
+                            <tr>
+                                <td>Name:</td>
+                                <td><input type="text" placeholder="TiaPuo" required></td>
+                            </tr>
+                            <tr>
+                                <td>E-mail:</td>
+                                <td><input type="email" placeholder="tiapuo@gmail.com" required></td>
+                            </tr>
+                            <tr>
+                                <td>Contact:</td>
+                                <td><input type="contact" placeholder="0546684004" required></td>
+                            </tr>
+                            <tr>
+                                <td>Gender:</td>
+                                <td>
+                                    <select name="gender" id="gender">
+                                        <option value="male" required>Male</option>
+                                        <option value="female" required>Female</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Hostels:</td>
+                                <td>
+                                    <select name="hostels" id="">
+                                        <option value="E-step" required>E-STEP</option>
+                                        <option value="forest" required>FOREST</option>
+                                        <option value="Get-fund" required>GET-FUND</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>level:</td>
+                                <td>
+                                    <select name="levels" id="levels">
+                                        <option value="L100" required>L100</option>
+                                        <option value="L200" required>L200</option>
+                                        <option value="L300" required>L300</option>
+                                        <option value="L400" required>L400</option>
+                                    </select>
+                                </td>
+                                <tr>
+                                    <td></td>
+                                    <td><input type="submit" placeholder="submit"></td>
+                                </tr>
+                            </tr>
+                        </table>
                     </form>
                 </div>
             </div>
@@ -140,17 +149,10 @@
             <div class="img_container ">
                 <div class="img_1 ">
                     <div><img src="./resources/images/hostelbeds2.jpg " alt="hostel image "></div>
-                    <h4>HOSTEL A</h4>
-                    <div style="display: flex;justify-content: center;align-items: center;padding: 5px 0; ">
-                        <div style="display:flex;align-items: center; ">
-                            <p>PRICE:</p>
-                            <!-- <ul>
-                                <li class="fi ">40.5
-                                    <ion-icon style="cursor: pointer; " name="caret-down-outline "></ion-icon>
-                                </li>
-                                <li>50.5</li>
-                                <li>60.5</li>
-                            </ul> -->
+                    <h4>Forest View Hostel</h4>
+                    <div class="price-rate">
+                        <div class="prices">
+                            <p>Fees:</p>
                             <select name="prices" id="prices">
                                 <option value="40.5">40.5</option>
                                 <option value="30.5">30.5</option>
@@ -168,17 +170,11 @@
                 </div>
                 <div class="img_2 ">
                     <div><img src="./resources/images/hostelbeds2.jpg " alt="hostel image "></div>
-                    <h4>HOSTEL A</h4>
-                    <div style="display: flex;justify-content: center;align-items: center;padding: 5px 0; ">
-                        <div style="display:flex;align-items: center; ">
-                            <p>PRICE:</p>
-                            <!-- <ul>
-                                <li class="fi ">40.5
-                                    <ion-icon style="cursor: pointer; " name="caret-down-outline "></ion-icon>
-                                </li>
-                                <li>50.5</li>
-                                <li>60.5</li>
-                            </ul> -->
+                    <h4>E-Step Hostel</h4>
+                    <div class="price-rate">
+                        <div class="prices">
+                            <p>Fees:</p>
+
                             <select name="prices" id="prices">
                                 <option value="40.5">40.5</option>
                                 <option value="30.5">30.5</option>
@@ -196,17 +192,11 @@
                 </div>
                 <div class="img_3 ">
                     <div><img src="./resources/images/hostelbeds2.jpg " alt="hostel image "></div>
-                    <h4>HOSTEL A</h4>
-                    <div style="display: flex;justify-content: center;align-items: center;padding: 5px 0; ">
-                        <div class="prices" style="display:flex;align-items: center; ">
-                            <p>PRICE:</p>
-                            <!-- <ul>
-                                <li class="fi ">40.5
-                                    <ion-icon style="cursor: pointer; " name="caret-down-outline "></ion-icon>
-                                </li>
-                                <li>50.5</li>
-                                <li>60.5</li>
-                            </ul> -->
+                    <h4>Get-Fund Hostel</h4>
+                    <div class="price-rate">
+                        <div class="prices">
+                            <p>Fees:</p>
+
                             <select name="prices" id="prices">
                                 <option value="40.5">40.5</option>
                                 <option value="30.5">30.5</option>
@@ -235,14 +225,23 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quisquam deleniti aut perferendis, possimus ratione aliquam unde. Nulla ad repellat magnam, alias ut similique! Delectus officia aspernatur, soluta id harum facilis, provident dignissimos
                 enim in beatae repudiandae molestiae deleniti reprehenderit! Tenetur rerum temporibus atque harum quaerat amet error vel ea.</p>
             <div class="icons ">
-                <ion-icon name="logo-twitter"></ion-icon>
-                <ion-icon name="logo-facebook"></ion-icon>
-                <ion-icon name="logo-whatsapp"></ion-icon>
-                <ion-icon name="logo-tiktok"></ion-icon>
+                <a href="#">
+                    <ion-icon name="logo-twitter"></ion-icon>
+                </a>
+                <a href="#">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+                <a href="#">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
+                <a href="#">
+                    <ion-icon name="logo-tiktok"></ion-icon>
+                </a>
 
             </div>
         </div>
     </footer>
+    <button class="top">Top</button>
 </body>
 <script src="resources/js/script.js "></script>
 
